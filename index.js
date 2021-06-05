e196ed3df3aaa81e4fa4ba409b9ba05e110f883f
AnneThor <anne.thor@gmail.com>
2021-06-05 12:02:47 -0700

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 const server = require('./src/server.js');
 require('dotenv').config();
 const PORT = process.env.PORT || 3333;
+const MONGO_URI = process.env.MONGO_URI;
 
 const mongoose = require('mongoose');
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.connect('mongodb://localhost:27017/api-server', options)
+mongoose.connect(MONGO_URI, options)
   .then(server.start(PORT));
