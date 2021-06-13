c7776beb3576246d954f5639373ad0f8d73bde34
AnneThor <anne.thor@gmail.com>
2021-06-13 16:19:45 -0700

    update server to include model; create acl based restrictions

---
+++ src/auth/auth-router.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+
+const User = require('./models/users.js');
+const basicAuth = require('./middleware/basic.js')
+const bearerAuth = require('./middleware/bearer.js')
+const permissions = require('./middleware/acl.js')
+
+authRouter.post('/signup', async (req, res) => {
+  try {
+    let user = new User(req.body);
+    const userRecord = await user.save();
+    const output = {
+      user: userRecord,
+      token: userRecord.token
+    };
+    res.status(201).json(output);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+authRouter.post('/signin', basicAuth, (req, res) => {
+  const user = {
+    user: req.user,
+    token: req.user.token
+  };
+  res.status(200).json(user);
+});
+
+authRouter.get('/users', bearerAuth, permissions('delete'), async (req, res) => {
+  const users = await User.find({});
+  const list = users.map(user => user.username);
+  res.status(200).json(list);
+});
+
+authRouter.get('/secret', bearerAuth, async (req, res) => {
+  res.status(200).send('Area for bearer authenticated users only');
+});
+
+authRouter.get('/editor', bearerAuth, permissions('create'), async (req, res) => {
+  res.status(200).send('Area for bearer authenticated users with editor and admin privs only');
+});
+
+authRouter.get('/admin', bearerAuth, permissions('delete'), async (req, res) => {
+  res.status(200).send('Area for bearer authenticated users with admin privs only');
+});
+
+module.exports = authRouter;
